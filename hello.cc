450e26b630d6846cead1336febb4594f6f87d2eb
Daniele Dalla Rosa <daniele.dallarosa@studenti.unitn.it>
2018-09-24 13:27:24 +0200

    Prima Lezione

---
+++ hello.cc
@@ -0,0 +1,14 @@
+//
+// Primo programma.
+// Stampa la stringa Hello World sul terminale.
+//
+
+using namespace std;
+#include <iostream>
+
+int main()
+{
+  cout << "Hello" << endl;
+    
+  return (0);
+}
